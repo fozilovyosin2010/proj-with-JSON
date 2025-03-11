73d16744dd851f6e98a563d99309dca0df6c493d
ozilovyosin2010 <umnovanika1@gmail.com>
2025-03-11 22:34:01 +0500

    starting

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
+
+// https://json-placeholder-pi.vercel.app/
+// https://jsonplaceholder.typicode.com/
+
+// for input -> https://mui.com/material-ui/react-text-field/
